6438245bda84948f3cb009d5eeeed2109e83add9
Mctripp <mctripp94@gmail.com>
2020-06-17 15:20:34 -0400

    Query card working, apiconfig for mtg

--- src/components/DevPage.js
+++ src/components/DevPage.js
@@ -1,11 +1,26 @@
 import React from 'react'
+// const mtg = require('mtgsdk')
+import { card } from 'mtgsdk'
 
 const DevPage = () => {
+  const [cardImg, setCardImg] = React.useState('')
+
+  React.useEffect(() => {
+    card.where({ subtypes: 'Golem' })
+      .then(cards => {
+        for (let i = 0; i < cards.length; i++) {
+          console.log(cards[i].name)
+        }
+        setCardImg(cards[23].imageUrl)
+      })
+  }, [])
+
   return (
     <React.Fragment>
       <p>
       This is the dev page.
       </p>
+      <img src={cardImg}/>
     </React.Fragment>
   )
 }
