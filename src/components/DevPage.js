b998a4547f0f0cd227b9324e6a0338999c17f23e
Mctripp <mctripp94@gmail.com>
2020-06-26 11:39:58 -0400

    Dynamic search display working

--- src/components/DevPage.js
+++ src/components/DevPage.js
@@ -1,66 +1,67 @@
-import React, { useState } from 'react'
-import CardDisplay from './CardDisplay/CardDisplay'
+import React from 'react'
+// import CardDisplay from './CardDisplay/CardDisplay'
 import MainSearch from './MainSearch/MainSearch'
 // const mtg = require('mtgsdk')
-import { card } from 'mtgsdk'
+// import { card } from 'mtgsdk'
 
 const DevPage = () => {
-  const [loaded, setLoaded] = useState(false)
-  const [cardsImg, setCardsImg] = useState([])
-  const [cardsName, setCardsName] = useState([])
-  const [cardsId, setCardsId] = useState([])
-  const [cardsSet, setCardsSet] = useState([])
+  // const [loaded, setLoaded] = useState(false)
+  // const [cardsImg, setCardsImg] = useState([])
+  // const [cardsName, setCardsName] = useState([])
+  // const [cardsId, setCardsId] = useState([])
+  // const [cardsSet, setCardsSet] = useState([])
 
-  React.useEffect(() => {
-    const cardImg = []
-    const cardName = []
-    const cardId = []
-    const cardSet = []
-    card.where({ subtypes: 'Cat' })
-      .then(cards => {
-        for (let i = 0; i < cards.length; i++) {
-          console.log(cards[i])
-          if (cards[i].imageUrl) {
-            cardImg.push(cards[i].imageUrl)
-            cardName.push(cards[i].name)
-            cardId.push(cards[i].id)
-            cardSet.push(cards[i].set)
-          }
-        }
-      })
-      .then(() => setCardsImg(cardImg))
-      .then(() => setCardsName(cardName))
-      .then(() => setCardsId(cardId))
-      .then(() => setCardsSet(cardSet))
-      .then(() => setLoaded(true))
-  }, [])
+  // React.useEffect(() => {
+  //   const cardImg = []
+  //   const cardName = []
+  //   const cardId = []
+  //   const cardSet = []
+  //   card.where({ name: 'Etali, Primal Storm' })
+  //     .then(cards => {
+  //       for (let i = 0; i < cards.length; i++) {
+  //         console.log(cards[i])
+  //         if (cards[i].imageUrl) {
+  //           cardImg.push(cards[i].imageUrl)
+  //           cardName.push(cards[i].name)
+  //           cardId.push(cards[i].id)
+  //           cardSet.push(cards[i].set)
+  //         }
+  //       }
+  //     })
+  //     .then(() => setCardsImg(cardImg))
+  //     .then(() => setCardsName(cardName))
+  //     .then(() => setCardsId(cardId))
+  //     .then(() => setCardsSet(cardSet))
+  //     .then(() => setLoaded(true))
+  // }, [])
 
   // After above axios, should store in a SearchResults page for navigation
 
-  if (!loaded) {
-    return (
-      <h1>Loading, this may take a minute...</h1>
-    )
-  } else {
-    return (
-      <React.Fragment>
-        <MainSearch/>
-        {cardsImg.map((cardImage, i) => {
-          const cardName = cardsName[i]
-          const cardId = cardsId[i]
-          const cardSet = cardsSet[i]
-          return (<CardDisplay
-            key = {i}
-            cardImage = {cardImage}
-            cardName = {cardName}
-            cardId = {cardId}
-            cardSet = {cardSet}
-          />
-          )
-        })}
-      </React.Fragment>
-    )
-  }
+  // if (!loaded) {
+  //   return (
+  //     <h1>Loading, this may take a minute...</h1>
+  //   )
+  // } else {
+
+  // {cardsImg.map((cardImage, i) => {
+  //   const cardName = cardsName[i]
+  //   const cardId = cardsId[i]
+  //   const cardSet = cardsSet[i]
+  //   return (<CardDisplay
+  //     key = {i}
+  //     cardImage = {cardImage}
+  //     cardName = {cardName}
+  //     cardId = {cardId}
+  //     cardSet = {cardSet}
+  //   />
+  //   )
+  // })}
+  return (
+    <React.Fragment>
+      <MainSearch/>
+    </React.Fragment>
+  )
 }
+// }
 
 export default DevPage
--- src/components/MainSearch/MainSearch.js
+++ src/components/MainSearch/MainSearch.js
@@ -1,23 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Dropdown } from 'semantic-ui-react'
+import cards from '../../MTGCards_IDs_Names_Sets'
 
-const cardOptions = [
-  { text: 'Mirri, Cat Warrior' },
-  { text: 'Etali, Primal Storm' },
-  { text: 'Avacyn, Angel of Hope' }
-]
+const cardIds = Object.keys(cards)
 
-const MainSearch = () => (
-  <Dropdown
-    button
-    className='icon'
-    floating
-    labeled
-    icon='card'
-    options={cardOptions}
-    search
-    text='Select Card'
-  />
-)
+const cardOptions = Object.values(cards).map((card, i) => {
+  return {
+    key: cardIds[i],
+    value: card.name,
+    text: card.name + ' (' + card.set + ')'
+  }
+})
+
+const MainSearch = () => {
+  const [query, setQuery] = useState('')
+
+  const handleChange = (event) => {
+    setQuery(event.target.value)
+  }
+
+  const dynamicSearch = () => {
+    return cardOptions.filter(card => card.text.includes(query))
+  }
+
+  if (query.length > 2) {
+    return (<Dropdown
+      button
+      className='icon'
+      floating
+      onSearchChange={handleChange}
+      labeled
+      options={dynamicSearch()}
+      search
+      text='Select Card'
+    />
+    )
+  } else {
+    return (<Dropdown
+      button
+      className='icon'
+      floating
+      onSearchChange={handleChange}
+      labeled
+      search
+      text='Select Card'
+    />
+    )
+  }
+}
 
 export default MainSearch
